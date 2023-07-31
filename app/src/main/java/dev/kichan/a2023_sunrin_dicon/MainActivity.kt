0adee09d98fcb8b9713501113c36d77b9bb066d4
바키찬 <ckstmznf@naver.com>
2023-07-31 22:29:32 +0900

    add:BaseActivity

--- app/src/main/java/dev/kichan/a2023_sunrin_dicon/MainActivity.kt
+++ app/src/main/java/dev/kichan/a2023_sunrin_dicon/MainActivity.kt
@@ -2,10 +2,11 @@ package dev.kichan.a2023_sunrin_dicon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.kichan.a2023_sunrin_dicon.databinding.ActivityMainBinding
+import dev.kichan.multiwallpaper.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
